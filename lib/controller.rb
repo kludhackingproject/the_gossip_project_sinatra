d0f89c00d69bae273443b08dd586977a0c846440
kludDesign <vincentgillibert@gmail.com>
2019-04-22 11:06:51 +0200

    Les premières views

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+  end
+
 end
